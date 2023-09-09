1950f7ae1aec1442955dcedf5a7f0e7d12c1c893
MauroPereira <mauro.a.pereira@gmail.com>
2023-09-09 00:46:14 -0300

    done: pregunta cantidad de host y acomoda para que quepan dentro de la ventana

--- map-tools-box/small_net_grapher/small_net_grapher.py
+++ map-tools-box/small_net_grapher/small_net_grapher.py
@@ -9,16 +9,26 @@ img_folder = os.path.join(os.path.dirname(__file__), "images")  # "images" es el
 # Crear un grafo
 G = nx.Graph()
 
-# Agregar nodos (switch y host)
-G.add_node("Switch")
-G.add_node("Host", ip="192.168.1.2")
+# Preguntar cuántos hosts están conectados
+num_hosts = int(input("Ingrese el número de hosts conectados: "))
 
-# Agregar una conexión entre el switch y el host
-G.add_edge("Switch", "Host")
+# Solicitar las direcciones IP de los hosts
+for i in range(1, num_hosts + 1):
+    ip = input(f"Ingrese la dirección IP del Host {i}: ")
+    G.add_node(f"Host {i}", ip=ip)
+
+# Agregar el switch
+G.add_node("Switch")
+
+# Conectar el switch con los hosts
+for i in range(1, num_hosts + 1):
+    G.add_edge("Switch", f"Host {i}")
 
 # Dibujar el grafo
+fig, ax = plt.subplots(figsize=(8, 8))  # Ajusta el tamaño de la figura
+
 pos = nx.spring_layout(G, seed=42)  # Usamos una semilla para que la disposición sea la misma cada vez
-nx.draw(G, pos, with_labels=False, node_size=1, node_color='skyblue')
+nx.draw(G, pos, with_labels=False, node_size=1, node_color='skyblue', ax=ax)  # Utiliza el mismo eje (ax)
 
 # Mostrar imágenes para los nodos
 for node, (x, y) in pos.items():
@@ -26,19 +36,19 @@ for node, (x, y) in pos.items():
         image_path = os.path.join(img_folder, "switch.png")
         imagebox = OffsetImage(plt.imread(image_path), zoom=0.5)
         ab = AnnotationBbox(imagebox, (x, y), frameon=False)
-        plt.gca().add_artist(ab)
+        ax.add_artist(ab)
         
         # Agregar etiqueta al switch
-        plt.text(x, y + 0.1, node, fontsize=12, ha="center")
-    elif node == "Host":
+        ax.text(x, y + 0.1, node, fontsize=12, ha="center")
+    elif node.startswith("Host"):
         image_path = os.path.join(img_folder, "host.png")
         imagebox = OffsetImage(plt.imread(image_path), zoom=0.5)
         ab = AnnotationBbox(imagebox, (x, y), frameon=False)
-        plt.gca().add_artist(ab)
+        ax.add_artist(ab)
         
         # Agregar etiqueta y dirección IP al host (moviendo la IP más abajo)
-        plt.text(x, y + 0.1, node, fontsize=12, ha="center")
-        plt.text(x, y - 0.2, G.nodes[node]["ip"], fontsize=12, ha="center")
+        ax.text(x, y + 0.1, node, fontsize=12, ha="center")
+        ax.text(x, y - 0.2, G.nodes[node]["ip"], fontsize=12, ha="center")
 
 plt.axis('off')  # Desactivar ejes
 plt.show()
